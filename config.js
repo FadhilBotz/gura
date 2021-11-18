ba737352cfd6b81eaf17cde16a29425dd0c64608
FadhilBotz <92239117+FadhilBotz@users.noreply.github.com>
2021-11-19 04:26:07 +0700

    Update setting.json

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ userbot = {
    waits: "Sedang di proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
    packname: "FADHIL-BOT OFFICIAL",
-   author: 'Arifi Razzaq',
+   author: 'Fadhil Hafuza',
     setting: {
     admin: "only admin",
     group: "only group",
